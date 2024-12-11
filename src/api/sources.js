4599207d5d061a536b09a77b04fdee0848c2941b
Shiyuan Miao <shiyuanm000@gmail.com>
2024-12-11 15:44:42 -0800

    add firebase and CI/CD

--- src/api/sources.js
+++ src/api/sources.js
@@ -4,7 +4,7 @@ import { SERVER_MAP } from "api/constants";
 
 export const getRentalListings = async () => {
   const source = SERVER_MAP.TREASURE_FINDER;
-  const route = "/rentListings";
+  const route = "/rentalListings";
   const url = createUrl(source, route);
   console.log(url);
   const response = await axios.get(url);
